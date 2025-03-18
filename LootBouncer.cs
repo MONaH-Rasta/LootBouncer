e0650237657ad8302440396fa29e0a4833a02729
MON@H Rasta <MONaH.Rasta@gmail.com>
2025-03-18 14:30:39 +0200

    v0.1.0

---
+++ LootBouncer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Oxide.Plugins
+{
+    [Info("Loot Bouncer", "Sorrow", "0.1.0")]
+    [Description("Empty the containers when players do not pick up all the items")]
+
+    class LootBouncer : RustPlugin
+    {
+        Dictionary<uint, int> lootEntity = new Dictionary<uint, int>();
+        private float _timeBeforeLootDespawn;
+
+        void OnLootEntity(BasePlayer player, BaseEntity entity)
+        {
+            if (entity == null || player == null || !player.IsAdmin) return;
+
+            var entityId = entity.net.ID;
+            var loot = entity.GetComponent<LootContainer>();
+            if (loot == null) return;
+            lootEntity.Add(entityId, loot.inventory.itemList.Count);
+        }
+
+        private void OnLootEntityEnd(BasePlayer player, BaseEntity entity)
+        {
+            if (entity == null || player == null || !player.IsAdmin) return;
+
+            var entityId = entity.net.ID;
+            var loot = entity.GetComponent<LootContainer>();
+            if (loot == null) return;
+
+            var originalValue = 0;
+            if (lootEntity.TryGetValue(entityId, out originalValue))
+            {
+                if (loot.inventory.itemList.Count != originalValue)
+                {
+                    timer.Once(_timeBeforeLootDespawn, () =>
+                    {
+                        if (loot == null) return;
+                        DropUtil.DropItems(loot?.inventory, loot.transform.position);
+                        BaseNetworkable.serverEntities.Find(entityId)?.Kill();
+                    });
+                }
+                lootEntity.Remove(entityId);
+            }
+        }
+
+        private new void LoadDefaultConfig()
+        {
+            PrintWarning("Creating a new configuration file");
+            Config.Clear();
+
+            Config["Time before the loot despawn in seconds"] = 10;
+
+            SaveConfig();
+        }
+
+        private void OnServerInitialized()
+        {
+            _timeBeforeLootDespawn = Convert.ToInt32(Config["Time before the loot despawn in seconds"]);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # LootBouncer
+
 Oxide plugin for Rust. Empty the containers when players do not pick up all the items
+
+## Configuration
+
+```json
+{
+  "Time before the loot containers are empties (seconds)": 30.0,//once an item has been remove from a crate - timer before create disappears
+  "Empty the entire junkpile when automatically empty loot": false,//once a crate or barrel is destroyed - does the entire junkpile disappear
+  "Empty the nearby loot when emptying junkpile": false,//if the junkpile is destroyed is the loot from the remaining crates and barrels dropped
+  "Time before the junkpile are empties (seconds)": 150.0,//once a crate or barrel is destroyed - timer before the entire junkpile disappears
+  "Slaps players who don't empty containers": false,
+  "Remove instead bouncing": false,//If true, the loot container items will no longer bounce
+  "Chat prefix": "[LootBouncer]:",
+  "Chat prefix color": "#00FFFF",
+  "Chat steamID icon": 0,
+  "Loot container settings": {       // If false, the container will not be emptied
+    "loot-barrel-1": true,
+    "loot-barrel-2": true,
+    "trash-pile-1": true
+  }
+}
+```
+
+## Credits
+
+**Sorrow**: original author of plugin
+**Arainrr**: previous maintainer
